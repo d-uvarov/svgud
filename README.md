b7b5de8a675cabdfbc9a51f8f2433289d62cc61d
Dmitry Uvarov <d.uvarov85@yandex.ru>
2018-05-27 01:59:47 +0300

    Changes to be committed: 	modified:   README.md 	modified:   index.js 	modified:   package.json

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # svgud
+Replaces a special tag in your vuejs template with your svg
+ 
+## Installation
+```bash
+$ npm install svgud
+```
+
+**webpack.config.js**
+```js
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {  // after vue-loader!
+        test: /\.vue$/,
+        loader: 'svgud',
+        options: {
+            path:  "./svg-folder"
+        }
+      }
+    ]
+  }
+}
+```
+
+## Usage
+
+```
+svg-folder/
+├── my.svg
+├── stars.svg
+└── child-folder
+    ├──my2.svg
+    └──some.svg
+webpack.config.js
+```
+
+**Vuejs component**
+```html
+<template>
+    <div>
+        <svgud name="my" />
+        <ul>
+            <li><svgud name="star" /></li>
+            <li><svgud name="child-folder/some" /></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "myComponent"
+    }
+</script>
+```
--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 var fs = require('fs');
+var path = require("path");
 var crypto = require('crypto');
 var files = [];
+var regExp = new RegExp(/<svgud(.+?)\/>/i);
 
 module.exports = function (content) {
     this.cacheable && this.cacheable();
 
-    var regExp = new RegExp(/<svgud(.+?)\/>/i);
+    var options = this.query;
+
+    if (!options.path || !options.path.trim()) {
+        throw new Error(
+            `Option "path" is required`
+        );
+    }
 
     if (regExp.test(content)) {
-        var options = this.query;
+        var svgFolder = path.resolve(options.path);
 
         content = content.replace(new RegExp(regExp, 'g'), function (tag) {
             var attrName = /name=(.)(.+?)\1/i.exec(tag);
-            // var attrPath = /path=(.)(.+?)\1/i.exec(tag);
 
             if (attrName == null) {
                 throw new Error(
@@ -20,15 +27,14 @@ module.exports = function (content) {
                 );
             }
 
-            var file = options.path + '/' + attrName[2] + '.svg';
-            var hashFile = crypto.createHmac('sha256', file);
+            var file = svgFolder + '/' + attrName[2] + '.svg';
+            var hashFile = crypto.createHmac('sha256', file).digest('hex');
 
             if (files.hasOwnProperty(hashFile)) {
                 return files[hashFile];
             }
 
             if (fs.existsSync(file)) {
-                console.log('File load!');
                 files[hashFile] = fs.readFileSync(file);
                 return files[hashFile];
             } else {
