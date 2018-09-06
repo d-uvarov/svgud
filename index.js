f0f38b3371321d6189117fbf3b7b4b2932b242f2
Dmitry Uvarov <d.uvarov85@yandex.ru>
2018-09-07 00:15:08 +0300

    Add random ids in svg

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 var fs = require('fs');
 var path = require("path");
 var crypto = require('crypto');
-var files = [];
+var files = {};
+var filesWithIds = {};
 var regExp = new RegExp(/<svgud(.+?)\/>/i);
+
+/**
+ *
+ * @param svg string
+ * @returns {Array}
+ */
+function findIdMatches(svg) {
+    let match = null;
+    let regx = new RegExp(/id="(.+?)"/gi);
+    let matches = [];
+
+    do {
+        match = regx.exec(svg);
+        if (match) {
+            matches.push(match[1]);
+        }
+    } while (match);
+
+    return matches;
+}
+
+
+/**
+ *
+ * @param svg string
+ * @param matches array
+ * @returns {*}
+ */
+function randomIds(svg, matches) {
+    for (let id of matches) {
+        let rnd = Math.random().toString(36).substring(2);
+        svg = svg.replace(new RegExp(`id="${id}"`, "g"), `id="${rnd}-${id}"`);
+        svg = svg.replace(new RegExp(`#${id}`, "g"), `#${rnd}-${id}`);
+    }
+
+    return svg;
+}
 
 module.exports = function (content) {
     this.cacheable && this.cacheable();
 
-    var options = this.query;
+    let options = this.query;
 
     if (!options.path || !options.path.trim()) {
         throw new Error(
@@ -16,10 +54,10 @@ module.exports = function (content) {
     }
 
     if (regExp.test(content)) {
-        var svgFolder = path.resolve(options.path);
+        let svgFolder = path.resolve(options.path);
 
         content = content.replace(new RegExp(regExp, 'g'), function (tag) {
-            var attrName = /name=(.)(.+?)\1/i.exec(tag);
+            let attrName = /name=(.)(.+?)\1/i.exec(tag);
 
             if (attrName == null) {
                 throw new Error(
@@ -27,16 +65,28 @@ module.exports = function (content) {
                 );
             }
 
-            var file = svgFolder + '/' + attrName[2] + '.svg';
-            var hashFile = crypto.createHmac('sha256', file).digest('hex');
+            let file = svgFolder + '/' + attrName[2] + '.svg';
+            let hashFile = crypto.createHmac('sha256', file).digest('hex');
 
             if (files.hasOwnProperty(hashFile)) {
                 return files[hashFile];
+            } else if (filesWithIds.hasOwnProperty(hashFile)) {
+                return randomIds(filesWithIds[hashFile], findIdMatches(filesWithIds[hashFile]));
             }
 
             if (fs.existsSync(file)) {
-                files[hashFile] = fs.readFileSync(file);
-                return files[hashFile];
+                let svgFile = fs.readFileSync(file);
+                let svg = svgFile.toString();
+                let matches = findIdMatches(svg);
+
+                if (matches.length) {
+                    filesWithIds[hashFile] = svg;
+
+                    return randomIds(filesWithIds[hashFile], matches);
+                } else {
+                    files[hashFile] = svg;
+                    return files[hashFile];
+                }
             } else {
                 throw new Error(
                     `Svg {${file}} not found`
